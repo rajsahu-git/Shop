fd76742744b9efcd644c62b339eecc007f2025d1
raju8824 <55652441+raju8824@users.noreply.github.com>
2022-12-07 23:13:50 +0530

    Fix Cart

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, Cart_item
 
@@ -33,10 +33,24 @@ def add_cart(request, product_id):
     return redirect('cart')
 # Create your views here.
 
-# def remove_cart(request, product_id):
-#     cart = Cart.objects.get(_cart_id=_card_id(request))
-#     product = get_object_or_404()
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product,id=product_id)
+    cart_item = Cart_item.objects.get(product=product,cart=cart)
+    if cart_item.quantity>1:
+        cart_item.quantity-= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
 
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product,id=product_id)
+    cart_item = Cart_item.objects.get(product=product,cart=cart)
+    cart_item.delete()
+    return redirect('cart')
 
 def cart(request, total=0, quantity=0, cart_item=None):
     try:
