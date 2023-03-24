3f20c825bda3eb68d193a21e3a572db3d65e2e81
raju8824 <55652441+raju8824@users.noreply.github.com>
2023-03-24 22:04:50 +0530

    Email verification

--- Pro/settings.py
+++ Pro/settings.py
@@ -143,3 +143,9 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'imraj823@gmail.com'
+EMAIL_HOST_PASSWORD = 'llpmvlqgusqayefz'
+EMAIL_USE_TLS = True
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,13 @@ from accounts.models import Account
 from .forms import RegistrationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 # Create your views here.
 def register(request):
     if request.method == 'POST':
@@ -19,6 +26,17 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username,password=password)
             user.phone = phone
             user.save()
+            current_site  = get_current_site(request)
+            mail_subject = "Please activate your account"
+            message = render_to_string('accounts/account_verification_email.html',{
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject,message,to=[to_email] )
+            send_email.send()
             messages.success(request,'Registration Success')
             return redirect('register')
     else:
@@ -46,3 +64,6 @@ def logout(request):
     auth.logout(request)
     messages.success(request,'You are logged out')
     return redirect('login')
+
+def activate(request):
+    return
